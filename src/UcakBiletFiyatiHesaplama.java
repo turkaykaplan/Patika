831cbf900689f72f1fed2ca0ef10559d31cfb538
Türkay Kaplan <82119064+turkaykaplan@users.noreply.github.com>
2022-11-07 14:07:20 +0300

    Update UcakBiletFiyatiHesaplama.java

--- src/UcakBiletFiyatiHesaplama.java
+++ src/UcakBiletFiyatiHesaplama.java
@@ -1,6 +1,6 @@
 /*
 - patika.dev
-Java ile mesafeye ve şartlara göre uçak bileti fiyatı hesaplayan programı yapın. 
+-- Java ile mesafeye ve şartlara göre uçak bileti fiyatı hesaplayan programı yapın. 
 Kullanıcıdan Mesafe (KM), yaşı ve yolculuk tipi  (Tek Yön, Gidiş-Dönüş) bilgilerini alın. Mesafe başına ücret 0,10 TL / km olarak alın. 
 İlk olarak uçuşun toplam fiyatını hesaplayın  ve sonrasında ki koşullara göre müşteriye aşağıdaki indirimleri uygulayın ;
 
