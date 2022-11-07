a7295637dd9569b235db67e6f083923250d6267d
Türkay Kaplan <82119064+turkaykaplan@users.noreply.github.com>
2022-11-07 23:04:35 +0300

    Update UcakBiletFiyatiHesaplama.java

--- src/UcakBiletFiyatiHesaplama.java
+++ src/UcakBiletFiyatiHesaplama.java
@@ -1,5 +1,5 @@
 /*
-- patika.dev
+/*- www.patika.dev
 -- Java ile mesafeye ve şartlara göre uçak bileti fiyatı hesaplayan programı yapın. 
 Kullanıcıdan Mesafe (KM), yaşı ve yolculuk tipi  (Tek Yön, Gidiş-Dönüş) bilgilerini alın. Mesafe başına ücret 0,10 TL / km olarak alın. 
 İlk olarak uçuşun toplam fiyatını hesaplayın  ve sonrasında ki koşullara göre müşteriye aşağıdaki indirimleri uygulayın ;
@@ -19,46 +19,46 @@ public class UcakBiletFiyatiHesaplama {
 
     public static void main(String[] args) {
         Scanner yazdir = new Scanner(System.in);
-        double yas, mesafe, toplam, indirim1,indirim2,indirim3,indirim4,indirim5,indirim6,indirim7, tip;
+        double age, distance, total, discount1,discount2,discount3,discount4,discount5,discount6,discount7, type;
 
         System.out.print("Mesafeyi giriniz (KM): ");
-        mesafe = yazdir.nextDouble();
+        distance = yazdir.nextDouble();
         System.out.print("Yaşınızı giriniz: ");
-        yas = yazdir.nextDouble();
+        age = yazdir.nextDouble();
         System.out.println("'1' - Tek Yön , '2' - Gidiş-Dönüş. Seçiniz: ");
-        tip = yazdir.nextDouble();
+        type = yazdir.nextDouble();
 
-        toplam = (double) (mesafe*0.10);                // Toplam bilet fiyatı üzerinden:
-        indirim1 = (double) (toplam - (toplam*0.5));    // 12 yaşından küçükler için %50 indirim
-        indirim2 = (double) (toplam - (toplam*0.1));    // 12-24 yaş arası için %10 indirim
-        indirim3 = (double) (toplam - (toplam*0.3));    // 65 yaş üstü için %30 indirim
-        indirim4 = (double) (toplam - (toplam*0.2));    // Gidiş-dönüş bileti (Tip=2) için  %20 indirim
-        indirim5 = (double) (indirim1 -(indirim1*0.2)); // Gidiş dönüş indirimi & 12 yaşından küçükler için %50 indirim
-        indirim6 = (double) (indirim2 -(indirim2*0.2)); // Gidiş dönüş indirimi & 12-24 yaş arası için %10 indirim
-        indirim7 = (double) (indirim3 -(indirim3*0.2)); // Gidiş dönüş indirimi & 65 yaş üstü için %30 indirim
+        total = (double) (distance*0.10);                // Toplam bilet fiyatı üzerinden:
+        discount1 = (double) (total - (total*0.5));    // 12 yaşından küçükler için %50 indirim
+        discount2 = (double) (total - (total*0.1));    // 12-24 yaş arası için %10 indirim
+        discount3 = (double) (total - (total*0.3));    // 65 yaş üstü için %30 indirim
+        discount4 = (double) (total - (total*0.2));    // Gidiş-dönüş bileti (Tip=2) için  %20 indirim
+        discount5 = (double) (discount1 -(discount1*0.2)); // Gidiş dönüş indirimi & 12 yaşından küçükler için %50 indirim
+        discount6 = (double) (discount2 -(discount2*0.2)); // Gidiş dönüş indirimi & 12-24 yaş arası için %10 indirim
+        discount7 = (double) (discount3 -(discount3*0.2)); // Gidiş dönüş indirimi & 65 yaş üstü için %30 indirim
 
-        if((tip == 1 || tip ==2) && (yas > 0 && mesafe> 0)){
+        if((type == 1 || type ==2) && (age > 0 && distance> 0)){
 
-            if(tip==1){
-               if(yas<12){
-                   System.out.println("Toplam tutar:"+ indirim1 +" TL");
-               } else if (yas < 24 && yas > 12) {
-                   System.out.println("Toplam tutar:"+ indirim2 +" TL");
-               } else if (yas > 24 && yas < 65) {
-                   System.out.println("Toplam tutar:"+ toplam +" TL");
-               }else if (yas > 65) {
-                   System.out.println("Toplam tutar:"+ indirim3 +" TL");
+            if(type==1){
+               if(age<12){
+                   System.out.println("Toplam tutar:"+ discount1 +" TL");
+               } else if (age < 24 && age > 12) {
+                   System.out.println("Toplam tutar:"+ discount2 +" TL");
+               } else if (age > 24 && age < 65) {
+                   System.out.println("Toplam tutar:"+ total +" TL");
+               }else if (age > 65) {
+                   System.out.println("Toplam tutar:"+ discount3 +" TL");
 
                }
-            } else if (tip==2) {
-                if(yas<12){
-                    System.out.println("Toplam tutar:"+ (indirim5*2) +" TL");
-                } else if (yas < 24 && yas > 12) {
-                    System.out.println("Toplam tutar:"+ (indirim6*2) +" TL");
-                } else if (yas > 24 && yas < 65) {
-                    System.out.println("Toplam tutar:"+ (indirim4*2) +" TL");
-                } else if (yas > 65) {
-                    System.out.println("Toplam tutar:"+ (indirim7*2) +" TL");
+            } else if (type==2) {
+                if(age<12){
+                    System.out.println("Toplam tutar:"+ (discount5*2) +" TL");
+                } else if (age < 24 && age > 12) {
+                    System.out.println("Toplam tutar:"+ (discount6*2) +" TL");
+                } else if (age > 24 && age < 65) {
+                    System.out.println("Toplam tutar:"+ (discount4*2) +" TL");
+                } else if (age > 65) {
+                    System.out.println("Toplam tutar:"+ (discount7*2) +" TL");
             }
         }
 
