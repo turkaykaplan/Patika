ea76a6ad8c8c37b976cd3e0d05c37be9cabf6eef
Türkay Kaplan <82119064+turkaykaplan@users.noreply.github.com>
2022-11-09 00:07:35 +0300

    Update GirilenSayiyaKadarUcveDordeBolunenSayilarinOrtalamasi.java

--- src/GirilenSayiyaKadarUcveDordeBolunenSayilarinOrtalamasi.java
+++ src/GirilenSayiyaKadarUcveDordeBolunenSayilarinOrtalamasi.java
@@ -1,22 +1,27 @@
+/*
+- www.patika.dev
+-- Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.
+*/
+
 import java.util.Scanner;
 
 public class GirilenSayiyaKadarUcveDordeBolunenSayilarinOrtalamasi {
     public static void main(String[] args) {
 
-        Scanner yazdir = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        int sayi,ortalama=0,toplam=0,adet=0;
+        int number,avg=0,total=0,counter=0;
 
         System.out.print("Sayı girin: ");
-        sayi = yazdir.nextInt();
+        number  = input.nextInt();
 
-        for(int i=1; i<=sayi; i++){
+        for(int i=1; i<=number; i++){
             if(i%3==0 && i%4==0){
-                toplam =+ i;
-                adet++;
+                total =+ i;
+                counter++;
             }
         }
-        ortalama = toplam/adet;
-        System.out.print(sayi +" sayısına kadar 3 ve 4'e tam bölünen sayıların ortalaması: "+ortalama);
+        avg = total/counter;
+        System.out.print(number +" sayısına kadar 3 ve 4'e tam bölünen sayıların ortalaması: "+avg);
     }
 }
