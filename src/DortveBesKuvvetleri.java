ddab16b94155751213ce90b713431ff5905efd6d
Türkay Kaplan <82119064+turkaykaplan@users.noreply.github.com>
2022-11-09 00:16:15 +0300

    Update DortveBesKuvvetleri.java

--- src/DortveBesKuvvetleri.java
+++ src/DortveBesKuvvetleri.java
@@ -1,19 +1,24 @@
+/*
+- www.patika.dev
+-- Java döngüler ile girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran program.
+*/
+
 import java.util.Scanner;
 
 public class DortveBesKuvvetleri {
     public static void main(String[] args) {
-        int sayi;
+        int number;
 
-        Scanner yazdir = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Sayı giriniz: ");
-        sayi = yazdir.nextInt();
+        number = input.nextInt();
 
-        for(int i=1; i<=sayi; i*=4){          // i = i*2
+        for(int i=1; i<=number; i*=4){          // i = i*2
             System.out.print("4'ün kuvveti: "+i);
             System.out.println();
         }
         System.out.println("--------------------");
-            for(int a=1; a<=sayi; a*=5){          // i = i*2
+            for(int a=1; a<=number; a*=5){          // i = i*2
                 System.out.print("5'in kuvveti: "+a);
                 System.out.println();
 
