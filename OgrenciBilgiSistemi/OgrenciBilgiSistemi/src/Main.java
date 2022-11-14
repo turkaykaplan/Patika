99c918ce37d8cfbe079dfd82d77d45a5ddfc31dc
Türkay Kaplan <82119064+turkaykaplan@users.noreply.github.com>
2022-11-14 12:52:58 +0300

    Add files via upload

---
+++ OgrenciBilgiSistemi/OgrenciBilgiSistemi/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Course mat = new Course("Matematik", "MAT101", "MAT");
+        Course fizik = new Course("Fizik", "FZK101", "FZK");
+        Course kimya = new Course("Kimya", "KMY101", "KMY");
+
+        Teacher t1 = new Teacher("Mahmut Hoca", "90550000000", "MAT");
+        Teacher t2 = new Teacher("Fatma Ayşe", "90550000001", "FZK");
+        Teacher t3 = new Teacher("Ali Veli", "90550000002", "KMY");
+
+        mat.addTeacher(t1);
+        fizik.addTeacher(t2);
+        kimya.addTeacher(t3);
+
+        Student s1 = new Student("İnek Şaban", 4, "140144015", mat, fizik, kimya);
+        s1.addBulkExamNote(50, 20, 40);
+        s1.addBulkSNote(80, 81, 82);
+        s1.isPass();
+
+        Student s2 = new Student("Güdük Necmi", 4, "2211133", mat, fizik, kimya);
+        s2.addBulkExamNote(100, 50, 40);
+        s2.addBulkSNote(10, 21, 99);
+        s2.isPass();
+
+        Student s3 = new Student("Hayta İsmail", 4, "221121312", mat, fizik, kimya);
+        s3.addBulkExamNote(30, 88, 60);
+        s3.addBulkSNote(38, 64, 79);
+        s3.isPass();
+
+    }
+}
---
+++ OgrenciBilgiSistemi/OgrenciBilgiSistemi/src/Student.java
@@ -0,0 +1,90 @@
+public class Student {
+    String name, stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage;
+    boolean isPass;
+
+
+    Student(String name, int classes, String stuNo, Course mat, Course fizik, Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+
+    public void addBulkExamNote(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.note = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.note = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.note = kimya;
+        }
+
+    }
+
+    public void addBulkSNote(int sMat, int sFizik, int sKimya) {
+        if (sMat >= 0 && sMat <= 100) {
+            this.mat.sNote = sMat;
+        }
+        if (sKimya >= 0 && sKimya <= 100) {
+            this.kimya.sNote = sKimya;
+        }
+        if (sFizik >= 0 && sFizik <= 100) {
+            this.fizik.sNote = sFizik;
+        }
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0) {
+            System.out.println("Notlar tam olarak girilmemiş.");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama: " + this.avarage);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti.");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+    }
+
+    public void calcAvarage() {
+        this.avarage = (this.fizik.note + this.kimya.note + this.mat.note) / 3;
+        double fizikAvg = this.fizik.note * 0.80 + this.fizik.sNote * 0.20;
+        double kimyaAvg = this.kimya.note * 0.80 + this.kimya.sNote * 0.20;
+        double matematikAvg = this.mat.note * 0.80 + this.mat.sNote * 0.20;
+        double grade = (fizikAvg + kimyaAvg + matematikAvg) / 3.0;
+
+    }
+
+    public boolean isCheckPass() {
+        calcAvarage();
+        return this.avarage > 55;
+    }
+
+    public void printNote() {
+        System.out.println("=========================");
+        System.out.println("Öğrenci : " + this.name);
+        System.out.println("Matematik Notu : " + this.mat.note);
+        System.out.println("Fizik Notu : " + this.fizik.note);
+        System.out.println("Kimya Notu : " + this.kimya.note);
+        System.out.println("Matematik Sözlü Notu : " + this.mat.sNote);
+        System.out.println("Fizik Sözlü Notu : " + this.fizik.sNote);
+        System.out.println("Kimya Sözlü Notu : " + this.kimya.sNote);
+    }
+
+}
