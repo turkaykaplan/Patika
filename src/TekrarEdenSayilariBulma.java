84a9c88c6c0dc012ee7be96113816141458a1b09
Türkay Kaplan <82119064+turkaykaplan@users.noreply.github.com>
2022-11-14 13:57:18 +0300

    Update TekrarEdenSayilariBulma.java

--- src/TekrarEdenSayilariBulma.java
+++ src/TekrarEdenSayilariBulma.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class TekrarEdenSayilariBulma {
 
-    static boolean isFind(int[] arr, int value){
+    static boolean find(int[] Array, int value){
 
-        for(int i : arr){
+        for(int i : Array){
             if (i == value) {
             return true;
             }
@@ -12,25 +12,22 @@ public class TekrarEdenSayilariBulma {
     }
 
     public static void main(String[] args) {
-        int startIndex = 0;
+        int index = 0;
         int[] list = {8, 7, 5, 16, 76, 8, 16,84,1782, 5,42, 84, 7,2, 3,1782, 9,42,6};
         int[] duplicate = new int[list.length];
 
         for (int i = 0; i < list.length; i++) {
             for (int j = 0; j < list.length; j++) {
                 if ((i != j) && (list[i] == list[j] && list[i]%2==0)) {
-                    if(!isFind(duplicate, list[i])) {
-                        duplicate[startIndex++] = list[i];
-                    }
-                    break;
+                    if(!find(duplicate, list[i])) {
+                        duplicate[index++] = list[i];
+                    }break;              
                 }
             }
         }
         System.out.println(Arrays.toString(list));
         for(int value : duplicate){
-            if(value !=0){
-                System.out.println(value);
-            }
+            if(value !=0)  System.out.println(value);                         
         }
     }
 }
